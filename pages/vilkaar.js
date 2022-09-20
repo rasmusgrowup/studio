06ae88e9f4b0e1152437f37643b21220eb999dff
Rasmus Andersen <rasmus@Rasmus.lan>
2022-09-20 18:07:59 +0200

    added terms to pages

---
+++ pages/vilkaar.js
@@ -0,0 +1,62 @@
+import scss from '../styles/vilkaar.module.scss'
+
+export default function Vilkaar() {
+	return (
+		<>
+			<h1 className={scss.h1}>
+				Generelle Forretningsvilkår
+			</h1>
+			<p className={scss.p}>
+				For at vi kan levere den bedste ydelse til Jer, har vi her nedskrevet vores generelle forretningsvilkår, som danner ramme for samarbejdet mellem Jer og os. Disse betingelser er som udgangspunkt de vilkår der gælder, når et samarbejde indgåes. Særlige vilkår, der ikke er en del af vores generelle forretningsvilkår, vil være beskrevet i den specifikke aftale.
+			</p>
+			<div className={scss.vilkaarGrid}>
+	            <span>Uforpligtende tilbud</span>
+	            <p>Et samarbejde med os starter typisk med et uforpligtende tilbud på en opgave. Som udgangspunkt mødes vi med Jer til en dybdegående snak om, hvad I ønsker af ydelser af os. Det er også her, vi vil præsentere vores tanker om, hvordan vi bedst kan hjælpe Jeres virksomhed. I de fleste tilfælde, kan vi give et tilbud efter det første møde, men i andre tilfælde, er projektet af en sådan karakter, at der er behov for en mere grundig undersøgelse.</p>
+
+	            <span>Aftalens indgåelse</span>
+	            <p>En aftale er som udgangspunkt indgået, når vi har sendt en ordrebekræftelse til Jer. Er det nødvendigt at annullere ordren, skal dette ske straks efter, at ordrebekræftelsen er sendt til Jer, og kan kun ske, hvis vi ikke har påbegyndt Jeres ordre. Ordrebekræftelsen tilsidesætter tidligere tilbud eller aftaler. <br /><br />
+	                Prisen på opgaven fremgår af ordrebekræftelsen. Alle priser vil være angivet ex. moms. I det omfang vi anvender betegnelsen <i>rammeestimat, overslagspris, budgettilbud</i> eller lign. er det fordi, det er vores bedste skøn over opgavens omfang og pris, og det skal forståes, at den endelige pris for disse typer estimater, i nogle tilfælde kan afvige i opadgående retning. Vi vil dog altid tage kontakt til Jer med det samme, hvis vi kan se at aftalens pris ikke kan overholdes.<br /><br />
+	                For at vi kan begynde på vores opgaver med det samme, og for at vi kan overholde vores deadlines, skal I ved aftalens indgåelse, levere al relevant materiale til brug for aftalen. I indestår for, at alle rettigheder – herunder immaterielle rettigheder – til materialet ejes af Jer, og at materialet derved ikke krænker en tredjemands rettigheder.</p>
+
+	            <span>Konsulentydelser</span>
+	            <p>Konsulentbistand ydes indenfor vores normale åbningstider kl. 9.00 – 16.30 på alle hverdage. I er naturligvis velkomne til at ringe til os indenfor åbningstiden, men venligst ikke udenfor de normale åbningstider, medmindre det er presserende. Priser på konsulentydelser vil være beskrevet i aftalekontrakten, og er som udgangspunkt faste gennem hele aftalens periode. Priserne kan ændre sig fra aftale til aftale - vi vil gøre Jer opmærksomme på ændringer, inden en ny aftale indgåes.<br /><br /> Konsulentydelser afregnes pr. påbegyndte time. Uden for normal arbejdstid afregnes pr. påbegyndte time + 100% oven i til en hver tid gældende timetakst.<br /><br />
+	                Kørsel afregnes pr. kørt km. Hertil kommer evt. bro-, færge eller flyudgifter tur/retur.</p>
+
+	            <span>Rettigheder </span>
+	            <p>Ophavsretten til de udviklede værker, forbliver som udgangspunkt hos Growup Aps, medmindre andet er aftalt. Det er ikke noget, i vil bemærke i den daglige drift, for I har naturligvis brugsret til det udviklede materiale, hvilket gør at Jeres virksomhed kan udnytte de udviklede materialer til eget brug, men I må ikke videresælge, udleje, ændre i eller kopiere materialerne, jævnfør ophavsretsloven.<br /><br />
+	                Det er almindeligt, at vi dokumenterer og deler arbejdsprocessen, bag  vores samarbejder og det udviklede materiale på vores SoMe-kanaler, som pr. d.d er Instagram og Behance, samt på vores hjemmeside www.growupstudio.dk. Giv os venligst besked ved aftalens indgåelse, hvis I ikke ønsker dette . Vi publicerer naturligvis aldrig følsomme informationer om Jeres virksomhed.<br /><br />
+	                Ønskes det udviklede materiale og dets projektfiler udleveret, kan det ske mod en ekstra betaling på 25% af den samlede produktionspris.</p>
+
+	            <span>Betaling</span>
+	            <p>Betalingsbetingelserne er netto kontant - 8 dage - medmindre andet er aftalt. Vi fakturer for medgået tid og eventuelle udlæg d. 15. i hver måned. Fakturaen kan sagtens fremsendes pr. brev, men det bliver dog for Jeres egen regning - fakturaen sendes som udgangspunkt pr. mail. Betales fakturaen ikke til tide, vil der komme en påmindelse pr. mail, og der vil tilskrives et rykkergebyr på 250,-.<br /><br />
+	            Vi kræver hel eller delvis forudbetaling ved påbegyndelse af projekter af større omfang. De specifikke betingelser, vil være beskrevet i aftalekontrakten, men typisk vil vi bede om en forudbetaling på 50% af det samlede beløb.<br /><br />
+	                Ved betalingsmisligholdelse er vi berettiget til at kræve vores omkostninger til inkasso ved advokat dækket fuldt ud.</p>
+
+	            <span>Vedligehold og drift</span>
+	            <p>Vi håber naturligvis på, at vores samarbejde fortsætter langt ud i fremtiden, også selvom produktet er afleveret og projektet afsluttet. Men er det ikke tilfældet, og er der ikke indgået anden aftale, er det Jeres eget ansvar at stå for vedligehold og drift af det udviklede materiale.<br /><br />
+	                Ved vedligehold og drift forståes eksempelvis, at den nødvendige software holdes opdateret til de nyeste versioner, at der tjekkes for fejl i materialerne eller indholdet på hjemmesiden, at hjemmesidens funktionaliteter og design testes ofte, at der føres back-up af det udviklede materiale m.m.<br /><br />
+	                Drift og vedligehold er en ydelse, som vi tilbyder, og en aftale herom indgåes gerne.</p>
+
+	            <span>Fejl og mangler</span>
+	            <p>Vi bestræber os på at aflevere vores produkter og ydelser fejlfrie, men det er naturligvis muligt, at der indsniger sig en fejl i ny og næ. I tilfælde af, at der er fejl i udviklede materiale, vil vi inden for rimelighedens grænser afhjælpe Jer med fejlen uden beregning. Det gør vi dog i udgangspunktet kun, hvis fejlen var tilstede da vi afleverede det udviklede materiale, eller afsluttede projektet.<br /><br />
+	                Vi forventer at I straks ved afslutning af projektet eller aflevering af materialer foretager en grundig undersøgelse af det udviklede materiale, og giver skriftlig besked om, at der er fundet en fejl eller mangel. Hvis der ikke foretages en undersøgelse med det samme, kan I ikke senere gøre manglen gældende. <br /><br />
+	                Vi forventer ligeledes at I tester, at alle funktionaliteter fungerer korrekt og efter hensigten.</p>
+
+	            <span>Ansvarsbegrænsning</span>
+	            <p>Vi er erstatningspligtig efter dansk rets almindelige regler. Dog er vi ikke i nogen tilfælde ansvarlige for driftstab, avancetab, følgeskader, andre indirekte tab eller tab af data.<br /><br />
+	                Det samlede økonomiske ansvar kan ikke overstige prisen på produktet eller ydelsen, der ligger til grund for erstatningen.</p>
+
+	            <span>Garanti</span>
+	            <p>Der tilbydes 30 dages garanti på vores produkter. Dette betyder at i har 30 dage til at gøre opmærksom på fejl eller mangler ved det afleverede produkt eller materiale, så vi kan udbedre fejlen eller manglen.<br /><br />
+	            Dette gælder dog ikke for tryksager. Det er jeres ansvar at tjekke for fejl og mangler, inden tryksagerne bliver sendt til tryk.</p>
+
+	            <span>Særlig om søgemaskineoptimering</span>
+	            <p>Vi garanterer ikke at i kan beholde jeres rangering i de forskellige søgemaskiner, ved publisering af en ny hjemmeside udviklet af Growup. Vi gør alt, hvad vi kan, for at i beholder jeres placering i søgemaskinerne, men det er desværre ikke muligt at garantere, at i umiddelbart efter lancering, har samme rangering som førhen.<br /><br />
+	            Vi sørger naturligvis for at sikre at der ikke opstår 404-sider, ved at lave redirects til jeres nye url-adresser.</p>
+
+	            <span>Fortrydelsesret</span>
+	            <p>Da alle opgaver og ordre som udgangspunkt er bestillingsvarer, er der ikke fortrydelsesret. Når et tilbud er accepteret er aftalen indgået og bindende. Igangsatte ordrer kan ikke annulleres.</p>
+	        </div>
+		</>
+	)
+}
